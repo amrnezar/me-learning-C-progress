de674fc9603e6f0a1e85d3721f5e6075a4804dba
amrnezar <146372001+amrnezar@users.noreply.github.com>
2025-03-13 14:14:49 +0800

    Add files via upload

---
+++ w3 school/index.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+   int main() {
+     cout << "Hello World!";
+     cout<<"\n Hi, Amr! good job\n";
+     cout<<3 << "\n";
+     cout << 3+5 << "\n";
+     cout<< 3*5 <<"\n\n";
+     cout<< 6 << endl;
+     //Another way to insert a new line, is with the endl manipulator
+     cout << 2;
+    return 0;
+   }
+// ctrl + C is to comment short cut , to uncomment ctrl + U
+
+// new line is "\n" and to but newline either in the same "" or 
+//make <<"\n" to make the new line after text
+// <"\n\n" gives you a blank line 
+//cout<< 3*5 <<"\n\n";
+// cout<< 6;
+//out put :
+//Hello World!
+// Hi, Amr! good job
+//3
+//8
+//15
+//
+//6
